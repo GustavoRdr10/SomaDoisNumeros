c96a348b2f15750588a744e41952c6bf6e23dd82
Gustavo Machado Rodrigues <guga10machado@hotmail.com>
2024-08-21 13:50:42 -0300

    first commit

---
+++ soma.js
@@ -0,0 +1,9 @@
+// Declaração de variáveis para armazenar os números
+let numero1 = 5; 
+let numero2 = 10; 
+
+// Calcula a soma dos dois números
+let soma = numero1 + numero2; 
+
+// Exibe o resultado da soma em um alerta
+alert("A soma é: " + soma); 
